b7c3d42ec9e84493e5166a9d30742f1923fbad69
dovix <kavoliunudovydas@gmail.com>
2024-01-18 23:04:37 +0200

    pradzia postu, photo modeliu, dokeri pasdariau

---
+++ src/main/java/com/example/springproject/models/Post.java
@@ -0,0 +1,32 @@
+package com.example.springproject.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    @OneToMany(mappedBy = "post")
+    private List<Photo> photos;
+//    private int likes;
+//
+//    private int dislikes;
+
+}
